91f0b4f3865e39e43f3996196d11adfecac35af0
AleCalz <calzadaloyaalejandra@gmail.com>
2024-06-05 16:00:36 -0600

    router-crud generation

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const kodersRouter = require('./routes/koders.router') 
 const mentorsRouter = require('./routes/mentors.router')
 const authRouter = require('./routes/auth.router')
+const genRouter = require('./routes/generation.router')
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.json())
 //despues montamos nuestro servidor
 app.use('/koders', kodersRouter)
 app.use('/auth', authRouter)
+app.use('/generation', genRouter)
 // app.use('/mentors', mentorsRouter)
 
 app.get('/', (request, response) => {
